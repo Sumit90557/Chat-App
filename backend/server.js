8df481d68052342963c00eb0f346f923f13442f7
Sumit90557 <sumitrajpoot38951@gmail.com>
2025-07-18 20:53:37 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
+import path from 'path' ;
 import dotenv from 'dotenv' ;
 dotenv.config() ; 
 import express from 'express' ; 
-
 import cookieParser from 'cookie-parser';
+
+
 //Routes 
 import userRoutes from './routes/user.routes.js';
 import authRoutes from './routes/auth.routes.js';
@@ -12,6 +14,7 @@ import connectToMongoDB from './db/connectToMongoDB.js';
 import { app, server } from './socket/socket.js';
 
 const PORT = process.env.PORT || 5000 ; 
+const __dirname = path.resolve() ; 
 
 
 app.use(express.json()) ; 
@@ -21,10 +24,11 @@ app.use("/api/auth" , authRoutes) ;
 app.use("/api/messages" , messageRoutes) ; 
 app.use("/api/users" , userRoutes) ; 
 
-// app.get("/" , (req , res)=>{
-//      res.send("hello sumit") ; 
-// }) ; 
+app.use(express.static(path.join(__dirname, "/frontend/dist"))) ;
 
+app.get("*", (req, res) =>{
+    res.sendFile(path.join(__dirname, "frontend" , "dist" , "index.html"));
+})
 
 
 server.listen(PORT, ()=>{
