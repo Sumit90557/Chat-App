df3d80964c33d0c69b82ef12a35cdd35aa4644ed
Sumit90557 <sumitrajpoot38951@gmail.com>
2025-07-19 00:40:21 +0530

    updated socket url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -19,7 +19,7 @@ const {authUser} = useAuthContext();
 
    useEffect(()=>{
     if(authUser){
-        const socket = io("http://localhost:5000",{
+        const socket = io("https://talk-net.onrender.com",{
             query:{
                 userId: authUser._id,
             }
